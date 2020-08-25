e04348df478c704d740fd1fa8ff6dab5404ff060
Alicja Michalak <alamichalak97@gmail.com>
2020-08-25 18:12:40 +0200

    changing icon using npm script

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
 import '../scss/main.scss';
+ 
+const glassNumber = document.querySelector('.hydrapp__number--js');
+const addGlassBtn = document.querySelector('.button--add--js');;
+const deleteGlassBtn = document.querySelector('.button--delete--js');
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
-console.log('HELLO 🚀')
