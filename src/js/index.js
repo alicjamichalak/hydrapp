864dd10c55af85d15b52fb742449c349503d1555
Alicja Michalak <alamichalak97@gmail.com>
2020-08-25 19:19:26 +0200

    adjusting styles modules and queryselectors in js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,19 @@
-import '../scss/main.scss';
- 
-const glassNumber = document.querySelector('.hydrapp__number--js');
-const addGlassBtn = document.querySelector('.button--add--js');;
-const deleteGlassBtn = document.querySelector('.button--delete--js');
+import "../scss/main.scss";
 
+const glassCounter = document.querySelector(".glass__counter--js");
+const addGlassBtn = document.querySelector(".button--add--js");
+const deleteGlassBtn = document.querySelector(".button--delete--js");
+
+let currentGlassCounter = 0;
+
+addGlassBtn.addEventListener("click", () => {
+  currentGlassCounter++;
+  glassCounter.innerHTML = currentGlassCounter;
+});
+
+deleteGlassBtn.addEventListener("click", () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+});
