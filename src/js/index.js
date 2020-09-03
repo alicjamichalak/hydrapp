d163adab2a73af1b16a27d6f6e8313719d3964b8
Alicja Michalak <alamichalak97@gmail.com>
2020-09-03 12:11:57 +0200

    adding glass counter to local storage

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,24 @@ import "../scss/main.scss";
 const glassCounter = document.querySelector(".glass__counter--js");
 const addGlassBtn = document.querySelector(".button--add--js");
 const deleteGlassBtn = document.querySelector(".button--delete--js");
+const key = new Date().toLocaleString().slice(0, 10);
 
 let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if(localStorageValue){
+  currentGlassCounter = localStorageValue;
+}else{
+  localStorage.setItem(key,0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
 
 addGlassBtn.addEventListener("click", () => {
   currentGlassCounter++;
   glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key,currentGlassCounter);
 });
 
 deleteGlassBtn.addEventListener("click", () => {
@@ -16,4 +28,5 @@ deleteGlassBtn.addEventListener("click", () => {
     currentGlassCounter--;
   }
   glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key,currentGlassCounter);
 });
